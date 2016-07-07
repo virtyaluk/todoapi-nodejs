9db35b4255c2fb7176c2eeb50d5f337c07fa0df1
Bohdan Shtepan <winsatana@ya.ru>
2016-07-07 23:56:49 +0300

    Adds base server logic

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express'),
+    mongoose = require('mongoose'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    app = express(),
+    config = require('./app/config/config');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+mongoose.connect(config.db);
+mongoose.connection.on('connected', () => { console.log('Mongoose default connection open to ' + config.db); });
+
+app.listen(config.port, (err) => {
+    if (err) { throw err; }
+
+    console.log("App listening on port " + config.port);
+});
